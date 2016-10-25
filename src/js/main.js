37ec46aa19fcea3c48a97db259a3f89d4e620303
Cameron Zandkarimi <camcamcz@gmail.com>
2016-10-25 16:33:53 -0400

    progress

--- src/js/main.js
+++ src/js/main.js
@@ -17,10 +17,12 @@ function getTrack (searchTerm) {
 
 getTrack("weezer").then(addToPage);
 
-$("#search-button").click(function (event) {
+
+$("form").submit(function (event) {
 	event.preventDefault();
 
 	var searchQuery = $("#query").val();
+	$(".searchresults").html("")
 	getTrack(searchQuery).then(addToPage);
 
 	console.log(searchQuery);
@@ -39,18 +41,35 @@ function addToPage (songs) {
 	    var itemHtml = searchTemplate(songs[i]);
  		$(".searchresults").append(itemHtml);
 	}
+	$(".stream-container").click(audioTemplate);
+}
+
+
+function audioTemplate (event) {
+	console.log(event);
+	var audioHtml = `
+	    <audio controls="controls" class="audio" autoplay>
+			<source src="${event.currentTarget.dataset.streamUrl}?client_id=${CLIENT_ID}"></source>
+        </audio>
+	`;
+	$(".audio-container").html(audioHtml);
+
 }
 
 
 
 function searchTemplate (song) {
-	//console.log(song);
-
+	console.log(song);
+	if(song.artwork_url === null){
+		song.artwork_url = "http://fillmurray.com/100/100"
+	}
 	return `
 	<div class="results">
 
 		
-		<img src="${song.user.avatar_url}">
+		<div class="stream-container" data-stream-url="${song.stream_url}"><img src="${song.artwork_url}" class="art">
+		<div class="songtitle">${song.title}</div>
+		</div>
 
 	</div>
 	`;
